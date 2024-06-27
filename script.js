6f7ec28b35023a811143aac30c3c4d7db77c62b4
Aayushgoyal00 <aayush04.ag@gmail.com>
2024-06-28 01:03:41 +0530

    Fixed errors

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ let temperature=document.getElementById("temperature")
 let Windspeed=document.getElementById("Windspeed")
 let Humidity=document.getElementById("Humidity")
 let Clouds=document.getElementById("Clouds")
+let error_para=document.getElementById("error_para")
 
 let inputValue = "";
 
@@ -99,13 +100,21 @@ async function fetchapi(present_city) {
             let result = await data.json();
             console.log(result);
             console.log(result.main);
-
+            
+            if(!error_para.classList.contains("hidden")){
+                error_para.classList.add("hidden")
+            }
             renderData(result)
             openModal()
             // console.log(result)
         } catch (err) {
             closeModal()
             console.log("Error:", err);
+            if(error_para.classList.contains("hidden")){
+                error_para.classList.remove("hidden")
+            }
+            error_para.textContent=err
+            alert("Type a valid area name")
         }
 }
 
@@ -144,6 +153,11 @@ async function fetchWeather(position) {
         console.log(data); // Log the data for debugging
     } catch (error) {
         console.error('Fetch error: ', error);
+        if(error_para.classList.contains("hidden")){
+            error_para.classList.remove("hidden")
+        }
+        error_para.textContent=error
+        alert("Type a valid area name")
     }
 }
 
@@ -188,105 +202,3 @@ updateClock();
 setInterval(updateClock, 1000);
 
 
-
-// (https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric);
-
-// (https://api.openweathermap.org/data/2.5/weather?q=goa&appid=f54c9746eeb575b169255821f07ad9a9&units=metric);
-
-
-// try{
-//     let city="goa"
-//     const apiKey="f54c9746eeb575b169255821f07ad9a9";
-// async function fetchApi(){
-//     let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
-//     let result=await data.json();
-//     console.log(result)
-// }
-// }catch(err){
-// console.log(Error,err)
-// }
-
-// fetchApi();
-
-// function openSearch(){
-//     let searchBar=document.getElementById("searchbar")
-//     searchBar.classList.remove("hidden");
-// }
-// function closeSearch(){
-//     let searchBar=document.getElementById("searchbar")
-//     if(!searchBar.classList.contains("hidden")){
-//         searchBar.classList.add("hidden");
-//     } 
-// }
-
-// let time_hour = new Date().getHours();
-// let time_minutes = new Date().getMinutes();
-// let time_seconds = new Date().getSeconds();
-// console.log(time_hour);
-
-
-// let hour=document.getElementById("hours");
-// let minutes=document.getElementById("minutes");
-// let seconds=document.getElementById("seconds");
-
-// let present_hour=time_hour
-// let present_minutes=time_minutes
-// let present_seconds=time_seconds
-
-// function increase( time_seconds){
-//     if(present_seconds===59){
-//         present_seconds=0
-//         if(present_minutes===59){
-//             present_minutes=0
-//             if(present_hour===23){
-//                 present_hour=0
-//             }
-//             else{
-//                 present_hour++
-//             }
-//             if(present_hour<10){
-//                 hour.textContent=`0${present_hour}`;
-//             }else{
-//                 hour.textContent=present_hour;
-//             }
-//         }
-//         else{
-//             present_minutes++
-//         }
-//         if(present_minutes<10){
-//             minutes.textContent=`0${present_minutes}`;
-//         }else{
-//             minutes.textContent=present_minutes;
-//         } 
-//     }
-//     else{
-//         present_seconds++
-//     }
-//     if(present_seconds<10){
-//         seconds.textContent=`0${present_seconds}`;
-//     }else{
-//         seconds.textContent=present_seconds;
-//     }
-    
-// }
-
-
-// if(time_hour<10){
-//     hour.textContent=`0${time_hour}`;
-// }else{
-//     hour.textContent=time_hour;
-// }
-
-// if(time_minutes<10){
-//     minutes.textContent=`0${time_minutes}`;
-// }else{
-//     minutes.textContent=time_minutes;
-// }
-
-// if(time_seconds<10){
-//     seconds.textContent=`0${time_seconds}`;
-// }else{
-//     seconds.textContent=time_seconds;
-// }
-
-// setInterval(() => increase(time_seconds), 1000);
